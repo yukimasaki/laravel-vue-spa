d3b2e0b8af222ad04b14fa8e9cb48db0be1094e4
yukimasaki <woodenhoe@gmail.com>
2022-08-01 15:51:24 +0900

    タスク一覧コンポーネント実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,28 @@
+import VueRouter from 'vue-router';
 import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
 
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent
+        },
+    ]
+});
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('header-component', HeaderComponent);
 
 const app = new Vue({
     el: '#app',
+    router
 });
